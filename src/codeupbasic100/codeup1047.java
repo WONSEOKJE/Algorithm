6590bbbdec09264c04f813097b0bb600343dc531
WONSEOKJE <nicholas4744@gmail.com>
2022-10-28 13:55:18 +0900

    20221028 study

---
+++ src/codeupbasic100/codeup1047.java
@@ -0,0 +1,11 @@
+package codeupbasic100;
+
+import java.util.Scanner;
+
+public class codeup1047 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println(a<<1);
+    }
+}
---
+++ src/codeupbasic100/codeup1053.java
@@ -0,0 +1,16 @@
+package codeupbasic100;
+
+import java.util.Scanner;
+
+public class codeup1053 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        if (a == 0) {
+            System.out.println(1);
+        }
+        else {
+            System.out.println(0);
+        }
+    }
+}
