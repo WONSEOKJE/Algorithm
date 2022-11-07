66d26d327629171ad5c685c3ced0cfa3717e3e5d
WONSEOKJE <nicholas4744@gmail.com>
2022-11-07 22:21:54 +0900

    20221107 study

--- src/boj/boj2439.java
+++ src/boj/boj2439.java
@@ -1,2 +1,19 @@
-package boj;public class boj2439 {
+package boj;
+
+import java.util.Scanner;
+
+public class boj2439 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for (int i = T-1; i >= 0; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = i; j < T; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 }
--- src/boj/boj2742.java
+++ src/boj/boj2742.java
@@ -1,2 +1,15 @@
-package boj;public class boj2742 {
+package boj;
+
+import java.util.Scanner;
+
+public class boj2742 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        int length = T;
+        for (int i = 0; i < length; i++) {
+            System.out.println(T);
+            T--;
+        }
+    }
 }
