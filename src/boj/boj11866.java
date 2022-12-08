64880176974e5ceb6c27b53ae2b32a6893e80796
WONSEOKJE <nicholas4744@gmail.com>
2022-12-08 16:59:08 +0900

    백준 11866번 문제 풀이

---
+++ src/boj/boj11866.java
@@ -0,0 +1,35 @@
+package boj;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class boj11866 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Queue<Integer> queue = new LinkedList<>();
+        StringBuilder sb = new StringBuilder();
+
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+
+        for (int i = 1; i <= N; i++) {
+            queue.add(i);
+        }
+
+        sb.append("<");
+
+        while(queue.size() > 1) {
+
+            for (int i = 0; i < K - 1; i++) {
+                int x = queue.poll();
+                queue.offer(x);
+            }
+
+            sb.append(queue.poll()).append(", ");
+        }
+
+        sb.append(queue.poll()).append(">");
+        System.out.println(sb);
+    }
+}
