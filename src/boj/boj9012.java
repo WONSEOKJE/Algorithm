1c14c985fd6b1387f577403894b6d928a54700b7
WONSEOKJE <nicholas4744@gmail.com>
2022-12-09 20:34:16 +0900

    백준 9012번 문재제풀이

---
+++ src/boj/boj9012.java
@@ -0,0 +1,44 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class boj9012 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; i++) {
+            boolean result = checkVPS(br.readLine());
+            if (result == true) {
+                sb.append("YES").append("\n");
+            } else {
+                sb.append("NO").append("\n");
+            }
+        }
+
+        System.out.println(sb);
+    }
+
+    static boolean checkVPS(String s) {
+
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(') {
+                stack.push(c);
+            } else if (stack.empty()) {
+                return false;
+            } else {
+                stack.pop();
+            }
+        }
+        if (stack.empty()) {
+            return true;
+        }
+        return false;
+    }
+}
