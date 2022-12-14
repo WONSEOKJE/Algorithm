8ce65c35cf08640cb36222a76ea8e6fc1c5f29b5
WONSEOKJE <nicholas4744@gmail.com>
2022-12-14 17:18:20 +0900

    inflearn Stringê° 강의 2번 문제풀이

---
+++ src/inflearn/string/lecture2.java
@@ -0,0 +1,25 @@
+package inflearn.string;
+
+import java.util.Scanner;
+
+public class lecture2 {
+    public static String solution(String str) {
+        String answer = "";
+        for (char c : str.toCharArray()) {
+            if (Character.isLowerCase(c)) {
+                answer += Character.toUpperCase(c);
+            }
+            else {
+                answer += Character.toLowerCase(c);
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        System.out.println(solution(str));
+    }
+}
