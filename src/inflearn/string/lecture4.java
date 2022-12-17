c0589bfed2b69a2aafde67ed1259c1b16f241879
WONSEOKJE <nicholas4744@gmail.com>
2022-12-17 21:24:34 +0900

    inflearn String 강의 4번 문ì문제풀이

---
+++ src/inflearn/string/lecture4.java
@@ -0,0 +1,28 @@
+package inflearn.string;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class lecture4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        String[] str = new String[N];
+        for (int i = 0; i < N; i++) {
+            str[i] = sc.next();
+        }
+        for (String s : solution(N, str)) {
+            System.out.println(s);
+        }
+
+    }
+
+    public static ArrayList<String> solution (int n, String[] str) {
+        ArrayList<String> answer = new ArrayList<>();
+        for (String s : str) {
+            String temp = new StringBuilder(s).reverse().toString();
+            answer.add(temp);
+        }
+        return answer;
+    }
+}
