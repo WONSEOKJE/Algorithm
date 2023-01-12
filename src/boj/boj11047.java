834ec13807f7829e24ed963ce5145f2a5aed33b8
WONSEOKJE <nicholas4744@gmail.com>
2023-01-12 13:14:07 +0900

    백준 11047번 문제풀이 - 동전0

---
+++ src/boj/boj11047.java
@@ -0,0 +1,32 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj11047 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int[] coin = new int[N];
+        int cnt = 0;
+
+        for (int i = 0; i < N; i++) {
+            coin[i] = Integer.parseInt(br.readLine());
+        }
+
+        for (int i = coin.length - 1; i >= 0; i--) {
+            if (K / coin[i] == 0) {
+                continue;
+            }
+
+            cnt += K / coin[i];
+            K = K % coin[i];
+        }
+
+        System.out.println(cnt);
+    }
+}
