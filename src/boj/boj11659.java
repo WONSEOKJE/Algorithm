22afdb12f21b8ca87da1f816c3f5c02313cd48fa
WONSEOKJE <nicholas4744@gmail.com>
2023-01-27 14:21:20 +0900

    백준 11659번 문제풀이 - 구간 합 구하기 4

---
+++ src/boj/boj11659.java
@@ -0,0 +1,32 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj11659 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N + 1];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i < N + 1; i++) {
+            arr[i] += arr[i - 1] + Integer.parseInt(st.nextToken());
+        }
+
+        for (int T = 0; T < M; T++) {
+            int sum = 0;
+            st = new StringTokenizer(br.readLine());
+            int i = Integer.parseInt(st.nextToken());
+            int j = Integer.parseInt(st.nextToken());
+            sum = arr[j] - arr[i - 1];
+            sb.append(sum).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
