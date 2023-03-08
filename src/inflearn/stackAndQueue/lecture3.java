22661c17933456093a4c04d3209b6def954e113b
WONSEOKJE <nicholas4744@gmail.com>
2023-03-08 22:31:25 +0900

    inflearn Stack,Queue 강의 3번 문제풀이

---
+++ src/inflearn/stackAndQueue/lecture3.java
@@ -0,0 +1,53 @@
+package inflearn.stackAndQueue;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class lecture3 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[][] board = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                board[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int M = Integer.parseInt(br.readLine());
+        int[] moves = new int[M];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            moves[i] = Integer.parseInt(st.nextToken());
+        }
+
+        System.out.println(solution(board,moves));
+    }
+
+    private static int solution(int[][] board, int[] moves) {
+        int answer = 0;
+        Stack<Integer> stack = new Stack<>();
+        for (int pos : moves) {
+            for (int i = 0; i < board.length; i++) {
+                if (board[i][pos-1]!=0) {
+                    int tmp = board[i][pos-1];
+                    board[i][pos-1] = 0;
+
+                    if (!stack.isEmpty() && tmp == stack.peek()) {
+                        answer += 2;
+                        stack.pop();
+                    } else {
+                        stack.push(tmp);
+                    }
+                    break;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
