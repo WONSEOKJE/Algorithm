4017ff462ba06a34d477e9cc67f4b3d1ba50b151
WONSEOKJE <nicholas4744@gmail.com>
2023-03-19 12:55:10 +0900

    inflearn Recursive,Tree,Graph 강의 3번

---
+++ src/inflearn/RecursiveAndTreeAndGraph/lecture3.java
@@ -0,0 +1,15 @@
+package inflearn.RecursiveAndTreeAndGraph;
+
+public class lecture3 {
+    public static void main(String[] args) {
+        System.out.println(DFS(5));
+    }
+
+    private static int DFS(int n) {
+        if (n == 1) {
+            return 1;
+        } else {
+            return n * DFS(n - 1);
+        }
+    }
+}
