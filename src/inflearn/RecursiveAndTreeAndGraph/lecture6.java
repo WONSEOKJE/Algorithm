1f77e3aca30b45c5fadb39cf97bcb9d8d213de06
WONSEOKJE <nicholas4744@gmail.com>
2023-03-21 17:24:31 +0900

    inflearn Recursive,Tree,Graph 강의 6번 (부분집합 구하기 DFS)

---
+++ src/inflearn/RecursiveAndTreeAndGraph/lecture6.java
@@ -0,0 +1,25 @@
+package inflearn.RecursiveAndTreeAndGraph;
+
+public class lecture6 {
+    static int n;
+    static int[] ch;
+    private static void DFS(int L) {
+        if (L == n+1) {
+            String tmp = "";
+            for (int i = 1; i <= n; i++) {
+                if (ch[i] == 1) tmp += (i + " ");
+            }
+            if (tmp.length() > 0) System.out.println(tmp);
+        } else {
+            ch[L] = 1;
+            DFS(L+1);
+            ch[L] = 0;
+            DFS(L+1);
+        }
+    }
+    public static void main(String[] args) {
+        n = 3;
+        ch = new int[n+1];
+        DFS(1);
+    }
+}
