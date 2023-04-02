612c9e5bfd8465b2e08c5925d4b92270105134da
WONSEOKJE <nicholas4744@gmail.com>
2023-04-02 15:44:31 +0900

    inflearn DFS와BFS 강의 6번 문제풀이

---
+++ src/inflearn/dfsAndbfs/lecture6.java
@@ -0,0 +1,43 @@
+package inflearn.dfsAndbfs;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class lecture6 {
+    static int n,m;
+    static int[] pm, ch, arr;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        ch = new int[n];
+        pm = new int[m];
+        DFS(0);
+    }
+
+    private static void DFS(int L) {
+        if (L == m) {
+            for (int x : pm) {
+                System.out.print(x + " ");
+            }
+            System.out.println();
+        } else {
+            for (int i = 0; i < n; i++) {
+                if (ch[i] == 0) {
+                    ch[i] = 1;
+                    pm[L] = arr[i];
+                    DFS(L+1);
+                    ch[i] = 0;
+                }
+            }
+        }
+    }
+}
